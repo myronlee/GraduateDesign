2bc83c270a6650838128799bf557421bd7846ac0
myronlee <1183989782@qq.com>
2015-11-20 17:30:13 +0800

    android 客户端

--- Amelie_Python/GrabCutDemo.py
+++ Amelie_Python/GrabCutDemo.py
@@ -4,19 +4,19 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('/Users/ligang/5_1.jpg')
+img = cv2.imread('/Users/ligang/Documents/Emilie/190_2.jpg')
 shape = img.shape[:2]
 mask = np.zeros(img.shape[:2], np.uint8)
 
 bgdModel = np.zeros((1, 65), np.float64)
 fgdModel = np.zeros((1, 65), np.float64)
 
-rect = (100, 10, 960, 1380)
+rect = (240, 350, 990, 1315)
 cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
 
 mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 img = img * mask2[:, :, np.newaxis]
 
-cv2.imwrite('/Users/ligang/5_1_cutted.jpg', img)
+cv2.imwrite('/Users/ligang/Documents/Emilie/190_2_cutted.jpg', img)
 
 plt.imshow(img), plt.colorbar(), plt.show()
