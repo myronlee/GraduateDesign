a081e9c1f8694a32c5ff1cc5bb1b444547e8035c
myronlee <1183989782@qq.com>
2015-12-03 15:25:08 +0800

    lbp code

---
+++ Amelie_Python/CaculateColorVector.py
@@ -0,0 +1,70 @@
+from PIL import Image
+from numpy import *
+import colorsys
+import datetime
+from PIL import ImageFile
+
+
+def compress_hsv(h, s, v):
+    if 316 <= h or h <= 20:
+        H = 0
+    elif 21 <= h <= 40:
+        H = 1
+    elif 41 <= h <= 75:
+        H = 2
+    elif 76 <= h <= 155:
+        H = 3
+    elif 156 <= h <= 190:
+        H = 4
+    elif 191 <= h <= 270:
+        H = 5
+    elif 271 <= h <= 295:
+        H = 6
+    elif 296 <= h <= 315:
+        H = 7
+    else:
+        assert False
+
+    if 0 <= s <= 0.2:
+        S = 0
+    elif 0.2 <= s <= 0.7:
+        S = 1
+    elif 0.7 <= s <= 1:
+        S = 2
+    else:
+        assert False
+
+    if 0 <= v <= 0.2:
+        V = 0
+    elif 0.2 <= v <= 0.7:
+        V = 1
+    elif 0.7 <= v <= 1:
+        V = 2
+    else:
+        assert False
+
+    return H, S, V
+
+def get_color_feature(image_path):
+    try:
+        img = Image.open(image_path)
+    except Exception, exception:
+        print exception
+        return []
+    except IOError, error:
+        print error
+        return []
+
+    wl = [0]*72
+    for count, (r, g, b) in img.getcolors(img.size[0]*img.size[1]):
+        h, s, v = colorsys.rgb_to_hsv(r/255.0, g/255.0, b/255.0)
+        h = int(h*360)
+
+        H, S, V = compress_hsv(h, s, v)
+
+        W = 9*H + 3*S + V
+        assert W < 72
+        wl[W] = wl[W] + count
+
+    print wl
+    print sum(wl)
